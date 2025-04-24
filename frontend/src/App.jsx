c94f58d1fa9fd75dd9296979d2a33c9e0bd8c457
Rishi <officialrishi4@gmail.com>
2025-04-24 21:17:29 +0530

    updated login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,49 @@
-import React from 'react';
-import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Blogs from './Pages/Blogs';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Dashboard from './Pages/Dashboard';
-import Creators from './Pages/Creators';
-import UpdateBlog from './dashboard/UpdateBlog';
-import Detail from './Pages/Detail';
-import Notfound from './Pages/Notfound';
-import { useAuth } from './context/AuthProvider';
-import { Toaster } from 'react-hot-toast';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import Blogs from './Pages/Blogs'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Dashboard from './Pages/Dashboard'
+import { useAuth } from './context/AuthProvider'
+import { Toaster } from 'react-hot-toast'
+import Creators from './Pages/Creators'
+import UpdateBlog from './dashboard/UpdateBlog'
+import Detail from './Pages/Detail'
+import Notfound from './Pages/Notfound'
 
 const App = () => {
   const location = useLocation();
-  const { blogs, isAunthicated } = useAuth();
+  const hidenavbarfooter = ["/dashboard","/login","/register"].includes(location.pathname);
 
-  const hideNavbarFooter = ["/dashboard", "/login", "/register"].includes(location.pathname);
+  const {blogs,isAunthicated} = useAuth();
+  console.log(blogs);
+  console.log(isAunthicated);
 
   return (
-    <div className="bg-white dark:bg-black text-black dark:text-white min-h-screen transition-colors duration-300">
-      {!hideNavbarFooter && <Navbar />}
+    <div>
+      {!hidenavbarfooter && <Navbar/>}
       <Routes>
-        <Route path="/" element={isAunthicated ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/creators" element={<Creators />} />
-        <Route path="/blog/update/:id" element={<UpdateBlog />} />
-        <Route path="/blog/:id" element={<Detail />} />
-        <Route path="*" element={<Notfound />} />
+        <Route exact path = "/" element = {<Home/>} />
+        <Route exact path = "/blogs" element = {<Blogs/>} />
+        <Route exact path = "/about" element = {<About/>} />
+        <Route exact path = "/contact" element = {<Contact/>} />
+        <Route exact path = "/login" element = {<Login/>} />
+        <Route exact path = "/register" element = {<Register/>} />
+        <Route exact path = "/dashboard" element = {<Dashboard/>} />
+        <Route exact path = "/creators" element = {<Creators/>} />
+        <Route exact path = "/blog/update/:id" element = {<UpdateBlog/>} />
+        <Route exact path = "/blog/:id" element = {<Detail/>} />
+        <Route path='*' element={<Notfound/>}/>
       </Routes>
-      <Toaster />
-      {!hideNavbarFooter && <Footer />}
+      <Toaster/>
+      {!hidenavbarfooter && <Footer/>}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
